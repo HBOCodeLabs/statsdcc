27736018e99a77eccf68a13b3f9bf8ffe5a84fa7
Russo, Robert (HBO) <Robert.Russo@hbo.com>
2018-04-16 15:37:59 -0400

    repeater check for keys existenace

--- lib/backends/repeater.cc
+++ lib/backends/repeater.cc
@@ -153,12 +153,16 @@ void Repeater::flush_stats(const Ledger& ledger, int flusher_id) {
   this->prefix_stats = ::config->name;
 
   // Statsd metrics
-  this->send(
-    this->prefix_stats + ".metrics_processed:" +
-    std::to_string(static_cast<long long int>(ledger.statsd_metrics["metrics_processed"])) + "|c")
-  this->send(
-    this->prefix_stats + ".processing_time:" +
-    std::to_string(static_cast<long long int>(ledger.statsd_metrics["processing_time"])) + "|ms")
+  if (ledger.statsd_metrics("metrics_processed") != ledger.statsd_metrics.end()) {
+      this->send(
+        this->prefix_stats + ".metrics_processed:" +
+        std::to_string(static_cast<long long int>(ledger.statsd_metrics["metrics_processed"])) + "|c");
+  }
+  if (ledger.statsd_metrics("processing_time") != ledger.statsd_metrics.end()) {
+      this->send(
+        this->prefix_stats + ".processing_time:" +
+        std::to_string(static_cast<long long int>(ledger.statsd_metrics["processing_time"])) + "|ms");
+    }
 
 }
 
